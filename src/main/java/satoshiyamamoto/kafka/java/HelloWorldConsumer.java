63628a914ebd7c863dad023886369b8a2fc7d456
Satoshi Yamamoto <satoshi_yamamoto@cyberagent.co.jp>
2017-07-13 19:55:22 +0900

    Initial Commit.

---
+++ src/main/java/satoshiyamamoto/kafka/java/HelloWorldConsumer.java
@@ -0,0 +1,41 @@
+package satoshiyamamoto.kafka.java;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class HelloWorldConsumer {
+
+	public static void main(String[] args) {
+		Properties props = new Properties();
+		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		props.put(ConsumerConfig.GROUP_ID_CONFIG, "HelloWorldConsumer");
+		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+		KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+		List<ConsumerRecord<String, String>> received = new ArrayList<>();
+
+		consumer.subscribe(Collections.singletonList("topic1"));
+
+		while (received.size() < 10) {
+			ConsumerRecords<String, String> records = consumer.poll(1000L);
+
+			records.forEach(received::add);
+		}
+	}
+
+}
---
+++ src/main/java/satoshiyamamoto/kafka/java/HelloWorldProducer.java
@@ -0,0 +1,29 @@
+package satoshiyamamoto.kafka.java;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class HelloWorldProducer {
+
+	public static void main(String[] args) {
+		Properties props = new Properties();
+		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		props.put(ProducerConfig.CLIENT_ID_CONFIG, "HelloWorldProducer");
+		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+		try (KafkaProducer<String, String> producer = new KafkaProducer<>(props)) {
+			producer.send(new ProducerRecord<>(
+					"topic1",
+					"Hello World kafka!")).get();
+		} catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
