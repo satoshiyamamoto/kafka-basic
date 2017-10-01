77572921955e2fc48532a01e2a3067f302ce43ac
Satoshi Yamamoto <satoshiyamamoto@me.com>
2017-10-01 12:56:52 +0900

    Optimized imports

--- src/main/java/satoshiyamamoto/kafka/java/HelloWorldConsumer.java
+++ src/main/java/satoshiyamamoto/kafka/java/HelloWorldConsumer.java
@@ -1,21 +1,12 @@
 package satoshiyamamoto.kafka.java;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 import java.util.Properties;
-import java.util.concurrent.ExecutionException;
 
 public class HelloWorldConsumer {
 
